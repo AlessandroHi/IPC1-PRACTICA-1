6b32e15675844c7d8c3219fb97055aa432339232
AlessandroHI <3028601340106@ingeniera.usac.edu.gt>
2021-06-03 18:43:44 -0600

    PRUEBA DE SUBIDA 1.0

--- PRACTICA1 IPC1/src/com/company/Main.java
+++ PRACTICA1 IPC1/src/com/company/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+    public static void menu(){
+        System.out.println("BIENVENIDO");
+        System.out.println("1. INGRESAR MATRIZ");
+    }
 }
